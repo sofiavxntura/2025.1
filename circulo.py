5d02f9f23f23da68527851efa3d110a54fe5bfd6
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 11:04:42 -0300

    Create circulo.py
    área do círculo

---
+++ circulo.py
@@ -0,0 +1,6 @@
+# ler o raio de um círculo, calcular e escrever a área
+import math
+raio = float(input("Raio do círculo (m): "))
+area = (math.pi)*(raio**2)
+print("Raio:",raio,"m")
+print("Área:",round(area,3),"m")
