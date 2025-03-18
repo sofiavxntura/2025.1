60244cd69bb42a8a6043ba9b688be9de27476af3
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 11:07:49 -0300

    Create farenheit.py
    conversor de celsius pra farenheit

---
+++ farenheit.py
@@ -0,0 +1,5 @@
+# ler uma temperatura em celsius e escrever em farenheit
+celsius = float(input("Temperatura em Celsius: "))
+farenheit = (9*celsius + 160)/5 
+print("Temperatura em Celsius:",celsius,"°C")
+print("Temperatura em Farenheit:",farenheit,"°F")
