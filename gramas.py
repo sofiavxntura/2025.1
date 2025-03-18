f216b57312e929339d610c879671d42e42ad6a38
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 10:57:50 -0300

    Create gramas.py
    converter kg pra g

---
+++ gramas.py
@@ -0,0 +1,4 @@
+# ler um valor em kg, converter p gramas e escrever o valor correspondente
+kg = float(input("Valor em kg: "))
+gramas = kg*1000
+print("Valor em kg:",kg,"\nValor em gramas:",gramas)
