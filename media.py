770d837f0801ce36c3f41d326f1230f9576673aa
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 09:40:27 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+# 1: Ler o valor de 3 números e escrever a média
+nota1 = int(input("Nota 1: "))
+nota2 = int(input("Nota 2: "))
+nota3 = int(input("Nota 3: "))
+media =(nota1 + nota2 + nota3)/3
+print("Média Final: ", round(media, 2))
