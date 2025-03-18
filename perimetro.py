f9711b2e546c39006133f5027c705b8568c20bb0
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 11:05:51 -0300

    Create perimetro.py
    calcular perimetro

---
+++ perimetro.py
@@ -0,0 +1,5 @@
+# ler a largura e a altura de um retângulo e escrever seu perímetro
+larg = float(input("Largura: "))
+alt = float(input("Altura: "))
+perimetro = 2*(larg+alt)
+print("Seu perímetro é de",perimetro,"m")
