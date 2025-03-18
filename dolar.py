815c7f710dcb5e4d58c53bd90ecc244d31bba812
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-18 11:03:52 -0300

    Create dolar.py
    converta dólares pra reais

---
+++ dolar.py
@@ -0,0 +1,5 @@
+# ler valor em dólares e a cotação do dólar em reais. calcule e escreva o valor convertido em reais
+dolar = float(input("Dólares: "))
+dol_comercial = dolar*5.69
+dol_turismo = dolar*5.93
+print("---------\nValor em reais\nD. Comercial: R$",dol_comercial,"\nDol. Turismo: R$",dol_turismo)
