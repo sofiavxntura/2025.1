a782d235a19c4e4856ea7cedae4d404cc7a5e313
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-25 10:06:21 -0300

    Create energiacinetica.py

---
+++ energiacinetica.py
@@ -0,0 +1,8 @@
+#energia cinética
+print("Calculadora de Energia Cinética!")
+massa = float(input("Massa (kg): "))
+velocidade = float(input("Velocidade (m/s): "))
+                         
+energia = ((velocidade**2)*massa)/2
+                         
+print("A energia cinética é de",energia,"J")
