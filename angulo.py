145fa41035578bb19c5c0062b32715dd32d4382a
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-25 09:51:20 -0300

    Create angulo.py

---
+++ angulo.py
@@ -0,0 +1,6 @@
+#angulo sup e comp
+angulo = int(input("Ângulo: "))
+comp = 90-angulo
+sup = 180-angulo
+
+print("Para o ângulo",angulo,"graus, temos:\nComplementar:",comp,"graus\nSuplementar:",sup,"graus")
