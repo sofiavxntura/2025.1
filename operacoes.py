d0d246f6b3a0c28a813b9c8e2585bbfe28c692ca
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-25 10:52:10 -0300

    Update operacoes.py

--- operacoes.py
+++ operacoes.py
@@ -1,1 +1,14 @@
+numero = int(input("Número de 3 dígitos: "))
+cent = numero//100
+dez = (numero - (cent*100))//10
+uni = (numero - (cent*100 + dez*10))
 
+soma_alg = cent+dez+uni
+inverso = uni*100 + dez*10 + cent
+menor = min(cent,dez,uni)
+maior = max(cent,dez,uni)
+dif = maior - menor # diferenca entre o maior e o menor digito
+
+print("Soma dos dígitos:",soma_alg)
+print("Inverso:",inverso)
+print("Diferença entre o maior e o menor dígito:",dif)
