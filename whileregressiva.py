268db9cbf1dce680256f7a8d84c55760a59768e7
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-04-09 11:53:18 -0300

    Update whileregressiva.py

--- whileregressiva.py
+++ whileregressiva.py
@@ -1,1 +1,10 @@
+number = int(input("Insira um número: "))
 
+if number < 0:
+    print("Insira um número positivo!")
+    
+else:
+    while number > 0:
+        print(number-1)
+        number = number-1
+print("acabou!")
