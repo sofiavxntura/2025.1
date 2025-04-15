64d0836576f02671bb330d0efe0dc5c04afd90d1
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-04-15 12:27:10 -0300

    Create strespaco.py

---
+++ strespaco.py
@@ -0,0 +1,9 @@
+espaco = ' '
+nome = input("Escreva algo! ")
+contador = 0
+
+for i in nome:
+    if i == espaco:
+        contador = contador + 1
+
+print(f"Você digitou um espaço {contador} vezes.")
