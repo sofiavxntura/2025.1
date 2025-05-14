7c9dc6381607d5fb9621d4aaf8758dade920f4b8
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-05-14 11:42:49 -0300

    Create vetorsegundomaior.py

---
+++ vetorsegundomaior.py
@@ -0,0 +1,17 @@
+lista = []
+parar = ''
+cont = 0
+
+while parar != 'p':
+    vec = int(input("Insira um número: "))
+    lista.append(vec)
+    parar = str(input("Insira p para parar."))
+    cont = cont + 1
+
+if cont < 1:
+    print("escreva mais números.")
+else:
+    crescente = sorted(lista)
+    maior2 = crescente[-2]
+    
+    print("O segundo maior número da lista é",maior2)
